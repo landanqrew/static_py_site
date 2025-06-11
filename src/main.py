6b752e0716272f38335e4029f7946b3fcb8f740f
Landan Quartemont <landan@grease.solutions>
2025-06-10 22:51:30 -0500

    enabled functionality for recursively generating pages in the source content folder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 from textnode import TextNode, TextType
 from htmlnode import HtmlNode, LeafNode, ParentNode
 from file_utilities import copy_folder_structure
-from generate_content import generate_page
+from generate_content import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -13,12 +13,8 @@ def main():
     # replace public folder with contents of static folder
     copy_folder_structure("static", "public")
     
-    print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 if __name__ == "__main__":
     print(main())
